1fef29c47582ba07a1f46dc3fabe79991c0e55b3
Sush1sui <jplovesraw@gmail.com>
2024-09-26 18:24:25 +0800

    PHP Router

--- 02-dynamic-web-app/functions.php
+++ 02-dynamic-web-app/functions.php
@@ -12,3 +12,21 @@ function urlIs($value)
 {
     $_SERVER['REQUEST_URI'] === $value;
 }
+
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require("views/{$code}.php");
+
+    die();
+}
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require($routes[$uri]);
+    } else {
+        abort();
+    }
+}
--- 02-dynamic-web-app/index.php
+++ 02-dynamic-web-app/index.php
@@ -1,15 +1,4 @@
 <?php
 
 require 'functions.php';
-
-$uri = $_SERVER['REQUEST_URI'];
-
-// dd($uri);
-
-if ($uri === '/') {
-    require('./controllers/index.php');
-} else if ($uri === '/about') {
-    require('./controllers/about.php');
-} else if ($uri === '/contact') {
-    require('./controllers/contact.php');
-}
+require 'Router.php';
