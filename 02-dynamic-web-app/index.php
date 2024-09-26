fd967c95a1f40eeef08221c5f164d81fbb2d9c09
Sush1sui <jplovesraw@gmail.com>
2024-09-26 20:03:37 +0800

    using PDO

--- 02-dynamic-web-app/index.php
+++ 02-dynamic-web-app/index.php
@@ -1,4 +1,17 @@
 <?php
 
 require 'functions.php';
-require 'Router.php';
+// require 'Router.php';
+
+// connect to MySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=demo;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
