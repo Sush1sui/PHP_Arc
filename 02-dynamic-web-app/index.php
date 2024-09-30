9cde7946d96b7558f1ba1e0709f9463389875454
Sush1sui <jplovesraw@gmail.com>
2024-09-30 09:47:48 +0800

    dynamic web app practice DONE

--- 02-dynamic-web-app/index.php
+++ 02-dynamic-web-app/index.php
@@ -1,15 +1,16 @@
 <?php
 
 require 'functions.php';
-require 'Database.php';
 // require 'Router.php';
+require 'Database.php';
 
-$db = new Database();
-$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [":id" => $id])->fetchAll();
 
 
 dd($posts);
-
-// foreach ($posts as $post) {
-//     echo "<li>" . $post['title'] . "</li>";
-// }
